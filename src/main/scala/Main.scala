9753f231a1afd9f065a8e0ae46664cd5ed6327a5
Pritam Kadam <kpritam@thoughtworks.com>
2021-05-09 22:43:51 +0530

    cleanup

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,7 +10,7 @@ import java.io.File
 
 object Logger {
   def log(prefixColor: String, prefix: String, msg: String): URIO[ZConsole, Unit] =
-    putStrLn(s"$prefixColor $prefix |${Console.RESET}${msg.replace("[info]", "")}")
+    putStrLn(s"$prefixColor$prefix |${Console.RESET}${msg.replace("[info]", "")}")
 }
 
 object Sbt {
@@ -20,20 +20,19 @@ object Sbt {
       wd: File
   ): ZIO[ZConsole with Blocking, CommandError, Fiber.Runtime[CommandError, ExitCode]] =
     for {
+      status  <- SubscriptionRef.make(false)
       process <- Command("sbt", cmd: _*).workingDirectory(wd).env(env).run
       fiber   <- process.exitCode.fork
       _       <- ZIO.succeed(Platform.addShutdownHook(() => unsafeRun(fiber.interrupt)))
-      stream  <- ZIO.succeed(process.stdout.linesStream)
-      ref     <- SubscriptionRef.make(false)
       _ <-
-        stream
+        process.stdout.linesStream
           .mapM { line =>
             val bool = line.contains(waitForOutput)
-            Logger.log(prefixColor, prefix, line) *> ref.ref.set(bool).when(bool)
+            Logger.log(prefixColor, prefix, line) *> status.ref.set(bool).when(bool)
           }
           .runDrain
           .forkDaemon
-      _ <- ref.changes.takeUntil(identity).runDrain
+      _ <- status.changes.takeUntil(identity).runDrain
     } yield fiber
 }
 
@@ -42,15 +41,12 @@ object Main extends zio.App {
   private val csw = new File(pwd + "/csw")
   private val esw = new File(pwd + "/esw")
 
-  private val program = {
-    for {
-      cswFiber <- Sbt.run(Console.YELLOW, "CSW-SERVICES", "Server online at", "csw-services/run start -c")(csw)
-      eswFiber <- Sbt
-        .run(Console.MAGENTA, "ESW-SERVICES", "Server online at", "esw-services/run start-eng-ui-services")(esw)
-      _ <- eswFiber.join
-      _ <- cswFiber.join
-    } yield ()
-  }
+  private val program = for {
+    cswFiber <- Sbt.run(Console.YELLOW, "CSW-SERVICES", "Server online at", "csw-services/run start -c")(csw)
+    eswFiber <- Sbt.run(Console.MAGENTA, "ESW-SERVICES", "Server online at", "esw-services/run start-eng-ui-services")(esw)
+    _        <- eswFiber.join
+    _        <- cswFiber.join
+  } yield ()
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
     program.exitCode
