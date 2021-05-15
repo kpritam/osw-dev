03dc6a3f795642418c67f6daeb83c30800d68545
Pritam Kadam <kpritam@thoughtworks.com>
2021-05-15 12:28:24 +0530

    Support --live arg

--- src/main/scala/dev/apps/TMTRunner.scala
+++ src/main/scala/dev/apps/TMTRunner.scala
@@ -4,10 +4,10 @@ import dev.utils.Git
 import dev.utils.Logger
 
 @main
-def TMTRunner(cmd: String) =
+def TMTRunner(cmd: String, live: String*) =
   cmd match
     case "init"              => Git.initSubmodules()
     case "update-submodules" => GitUpdateSubmodules.update()
     case "print-versions"    => Versions.prettyPrint()
-    case "start"             => ServicesLauncher.launch(false)
+    case "start"             => ServicesLauncher.launch(live.headOption.contains("--live"))
     case invalid             => Logger.logRed(s"$invalid command not supported!")
