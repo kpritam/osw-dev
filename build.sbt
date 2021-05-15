d323907ebdee689a8f1f34d9bb743e39c7bed52c
Pritam Kadam <kpritam@thoughtworks.com>
2021-05-15 23:29:35 +0530

    Use zio-cli

--- build.sbt
+++ build.sbt
@@ -1,6 +1,13 @@
-scalaVersion := "3.0.0"
-name := "tmt-mono"
-version := "1.0"
+inThisBuild(
+  List(
+    scalaVersion := "3.0.0",
+    name := "tmt-mono",
+    version := "1.0"
+  )
+)
 
-libraryDependencies += ("com.lihaoyi" %% "os-lib" % "0.7.3").cross(CrossVersion.for3Use2_13)
-
+lazy val root = (project in file("."))
+  .settings(
+    libraryDependencies += ("com.lihaoyi" %% "os-lib" % "0.7.3").cross(CrossVersion.for3Use2_13)
+  )
+  .dependsOn(ProjectRef(uri("git://github.com/zio/zio-cli.git"), "zioCliJVM"))
--- src/main/scala/dev/apps/TMTRunner.scala
+++ src/main/scala/dev/apps/TMTRunner.scala
@@ -3,11 +3,63 @@ package dev.apps
 import dev.utils.Git
 import dev.utils.Logger
 
-@main
-def TMTRunner(cmd: String, live: String*) =
-  cmd match
-    case "init"              => Git.initSubmodules()
-    case "update-submodules" => GitUpdateSubmodules.update()
-    case "print-versions"    => Versions.prettyPrint()
-    case "start"             => ServicesLauncher.launch(live.headOption.contains("--live"))
-    case invalid             => Logger.logRed(s"$invalid command not supported!")
+import zio.cli.{Args, CliApp, Command, Exists, Options}
+import zio.cli.HelpDoc
+import zio.cli.HelpDoc.Span.text
+
+import zio.*
+import zio.blocking.effectBlocking
+import zio.console.putStrLn
+
+enum TMTRunnerCommand:
+  case Start(live: Boolean)
+  case PrintVersions
+  case UpdateSubmodules
+  case Init
+
+object GitExample extends App:
+  val liveFlag: Options[Boolean] = Options.bool("live", false)
+
+  val start =
+    Command("start", liveFlag, Args.none, HelpDoc.Paragraph(text("Start CSW and ESW Services")))
+      .map { case (live, _) => TMTRunnerCommand.Start(live) }
+
+  val init = Command(
+    "init",
+    Options.none,
+    Args.none,
+    HelpDoc.Paragraph(text("Initializes submodules for the first time"))
+  ).map(_ => TMTRunnerCommand.Init)
+
+  val printVersions = Command(
+    "print-versions",
+    Options.none,
+    Args.none,
+    HelpDoc.Paragraph(text("Prints versions compatibility table"))
+  ).map(_ => TMTRunnerCommand.PrintVersions)
+
+  val updateSubmodules = Command(
+    "update-submodules",
+    Options.none,
+    Args.none,
+    HelpDoc.Paragraph(text("Updates all the submodules"))
+  ).map(_ => TMTRunnerCommand.UpdateSubmodules)
+
+  val tmt = Command("tmt", Options.none, Args.none, HelpDoc.Header(text("TMT runner"), 2))
+    .subcommands(start | init | printVersions | updateSubmodules)
+    .map(_._2)
+
+  val app = CliApp(
+    "TMT Runner application",
+    "0.1.0",
+    text("a client for running TMT specific services"),
+    tmt,
+    execute = {
+      case TMTRunnerCommand.Init             => effectBlocking(Git.initSubmodules())
+      case TMTRunnerCommand.UpdateSubmodules => effectBlocking(GitUpdateSubmodules.update())
+      case TMTRunnerCommand.PrintVersions    => effectBlocking(Versions.prettyPrint())
+      case TMTRunnerCommand.Start(live)      => effectBlocking(ServicesLauncher.launch(live))
+    }
+  )
+
+  override def run(args: List[String]) = app.run(args)
